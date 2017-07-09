2e6a145393189d6fb6e11e3886f45bb61fba54ee
root <root@localhost.localdomain>
2017-07-09 12:32:50 -0400

    fix main.c

--- main.c
+++ main.c
@@ -1,45 +1,103 @@
 #include <stdio.h>
 #include <pcap.h>
-#include <arpa/inet.h>
+#include <stdint.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <stdlib.h>
-#include <string.h>
 
-	struct ether_header{
-		u_char dst[6];
-		u_char src[6];
-		u_char type[2];
-	};
+#define ETHER_SIZE 14
+#define ETHER_ADDR_LEN 6
 
-	struct sniff_ip {
-		u_char ip_vhl;		/* version << 4 | header length >> 2 */
-		u_char ip_tos;		/* type of service */
-		u_short ip_len;		/* total length */
-		u_short ip_id;		/* identification */
-		u_short ip_off;		/* fragment offset field */
-	#define IP_RF 0x8000		/* reserved fragment flag */
-	#define IP_DF 0x4000		/* dont fragment flag */
-	#define IP_MF 0x2000		/* more fragments flag */
-	#define IP_OFFMASK 0x1fff	/* mask for fragmenting bits */
-		u_char ip_ttl;		/* time to live */
-		u_char ip_p;		/* protocol */
-		u_short ip_sum;		/* checksum */
-		struct in_addr ip_src,ip_dst; /* source and dest address */
-	};
+u_int16_t ip_size = 0;
+u_int16_t tcp_size = 0;
 
-	struct tcp_header{
-		u_short tcp_src;
-		u_short tcp_dst;
-		u_char tcp_seq[4];
-		u_char tcp_acknum[4];
-		u_char tcp_offetc[4];
-		u_char chksum[2];
-		u_char ptr[2];
-		u_char option[4];
-	};
+struct ether_header{
+	u_int8_t ether_dhost[ETHER_ADDR_LEN];
+	u_int8_t ether_shost[ETHER_ADDR_LEN];
+	u_int16_t ether_type;
+};
+
+struct ip_header{
+	u_int8_t ip_hl:4,ip_v:4;
+	u_int8_t ip_tos;
+	u_int16_t ip_len;	// ip total length
+	u_int16_t ip_id;
+	u_int16_t ip_off;
+	u_int8_t ip_ttl;
+	u_int8_t ip_proto;
+	u_int16_t ip_sum;
+	struct in_addr ip_src,ip_dst;
+};
+
+struct tcp_header{
+	u_int16_t tcp_src;
+	u_int16_t tcp_dst;
+	u_int32_t tcp_seq;
+	u_int32_t tcp_ack;
+	u_int8_t tcp_rev:4,tcp_off:4;
+	u_int8_t tcp_flag;
+	u_int16_t tcp_win;
+	u_int16_t tcp_sum;
+	u_int16_t tcp_ptr;
+};
 
 
-int main(int argc, char *argv[])
+int func_ether(u_char* packet, struct ether_header *ether){
+	ether = (struct ether_header*)(packet);
+	if(ntohs(ether->ether_type) == 0x0800){
+	
+	printf("[+] Ethernet header information...\n");
+	printf("Destination MAC : ");
+	for(int i=0;i<ETHER_ADDR_LEN;i++){
+		printf("%02x",ether->ether_dhost[i]);
+		if(i<ETHER_ADDR_LEN-1) printf(":");
+		else printf("\n");
+	}
+	printf("Source MAC : ");
+	for(int i=0;i<ETHER_ADDR_LEN;i++){
+		printf("%02x",ether->ether_shost[i]);
+		if(i<ETHER_ADDR_LEN-1) printf(":");
+		else printf("\n");
+	}
+		return 1;
+	}
+	else{
+		return -1;
+	}
+}
+
+
+int func_ip(u_char* packet, struct ip_header *ip){
+	char buf[INET_ADDRSTRLEN];
+	ip = (struct ip_header*)(packet+ETHER_SIZE);
+	if(ip->ip_proto == 0x06){
+	printf("\n\n[+] IP header information...\n");
+	printf("source IP : %s\n",inet_ntop(AF_INET,&(ip->ip_src.s_addr),buf,INET_ADDRSTRLEN));
+	printf("destination IP : %s\n",inet_ntop(AF_INET,&(ip->ip_dst.s_addr),buf,INET_ADDRSTRLEN));
+	ip_size = ip->ip_hl*4;
+	printf("ip_size : %d\n",ip_size);
+	}
+	else{
+		return -1;
+	}
+}
+
+void func_tcp(u_char* packet, struct tcp_header *tcp){
+	tcp = (struct tcp_header*)(packet+ETHER_SIZE+ip_size);
+	tcp_size = tcp->tcp_off*4;
+	printf("\n\n[+] TCP header information...\n");
+	printf("TCP source port : %d\n",ntohs(tcp->tcp_src));
+	printf("TCP destination port : %d\n",ntohs(tcp->tcp_dst));
+	printf("TCP size : %d\n",tcp_size);
+}
+
+void func_data(u_char* packet){
+	u_char *data = packet+ETHER_SIZE+ip_size+tcp_size;
+	printf("\n\n[+] data section...\n");
+	printf("%s",data);
+}
+
+int main(int argc, char* argv[])
 {
 	char *dev, errbuf[PCAP_ERRBUF_SIZE];
 	pcap_t *handle;
@@ -50,15 +108,18 @@ int main(int argc, char *argv[])
 	struct bpf_program fp;			/* compiled filter program (expression) */
 	bpf_u_int32 mask;			/* subnet mask */
 	bpf_u_int32 net;			/* ip */
+	u_char *packet_ptr;
 
-	struct ether_header ether;
-	struct sniff_ip *ip;
+	struct ether_header *ether;
+	struct ip_header *ip;
 	struct tcp_header *tcp;
 
 	u_char *data;
 	int opt = 1;
-	
-	dev = pcap_lookupdev(errbuf);
+
+	//dev = pcap_lookupdev(errbuf);
+	//dev = "dum0";
+	dev = argv[1];
 
 	if(dev == NULL){
 		printf("[+] Cannot find default device!\n");
@@ -91,69 +152,27 @@ int main(int argc, char *argv[])
 	}
 
 	while(1){
-	packet = pcap_next(handle,&header);
-	printf("=====================================================\n");
-	printf("[+] Jacked packet length : %d\n\n", header.len);
-	for(int i=0;i<header.len;i++){
-		if(i<6){
-			if(idx == 0)
-				printf("[+] DST MAC : ");
-			printf("%02x",*packet);
-			ether.dst[idx++] = *packet++;	
-			if(idx != 6) printf(":");
-			if(idx == 6) {
-				idx = 0;
-				printf("\n");
-			}
+		printf("\n\n[+] capture the packet\n\n");
+		packet = pcap_next(handle,&header);	
+		packet_ptr = packet;
+		for(int i=0;i<header.len;i++){
+			printf("%02x ",*(packet_ptr++));
+			if(i%16==0 && i!=0) printf("\n");
 		}
-		else if(i<12){
-			if(idx == 0)
-				printf("[+] SRC MAC : ");
-			printf("%02x",*packet);	
-			ether.src[idx++] = *packet++;
-			if(idx != 6) printf(":");
-			if(idx == 6){
-			 	idx = 0;
-				printf("\n");
-			}
-		}
-		else if(i<14){
-			ether.type[idx++] = *packet++;
-			if(idx == 2){
-				idx = 0;
-				printf("\n");
-			}
-		}
-		else if(i<34){
-			ip = (struct sniff_ip*)(packet);
-			printf("[+] SRC IP : %s\n",inet_ntoa(ip->ip_src));
-			printf("[+] DST IP : %s\n",inet_ntoa(ip->ip_dst));
-			i = 34;
-			packet += 20;
-			printf("\n");
-		}
-		else if(i<54){
-			tcp = (struct tcp_header*)(packet);
-			printf("[+] SRC TCP : %d\n",ntohs(tcp->tcp_src));
-			printf("[+] DST TCP : %d\n",ntohs(tcp->tcp_dst));
-			i = 54;
-		}
-		else if(i >= 54){
-			data = (u_char *)(packet+20);
-			printf("\n[+]Data section\n");
-			printf("%s\n",data);
-			printf("\n[+] Press 1 and Enter to continue or Press 2 and Enter to exit\n");
-			scanf("%d",&opt);
-			if(opt == 2){
-				printf("\n");
-				pcap_close(handle);
-				return 0;
-			}
-			break;
-		}
-	}
-	}
 
+		printf("\n");
 
+		if(func_ether(packet,ether) == -1) continue;
+		if(func_ip(packet,ip) == -1) continue;
+		func_tcp(packet,tcp);
+		func_data(packet);
+
+		printf(">>>>> ");
+		scanf("%d",&opt);
+		if(opt == 1) break;
+		else continue;
+	}
 	return 0;
 }
+
+	 
